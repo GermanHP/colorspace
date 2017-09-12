e10054ead26f474d36541804a6cac2ab4a0b8ae6
German D. Hernández <gdhernandezp@gmail.com>
2017-09-12 16:11:26 -0600

    Migrations y seeders

--- database/migrations/2017_09_12_153447_TipoTelefono.php
+++ database/migrations/2017_09_12_153447_TipoTelefono.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Promociones extends Migration
+class TipoTelefono extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class Promociones extends Migration
      */
     public function up()
     {
-        Schema::create('noticias', function (Blueprint $table) {
+        Schema::create('tipotelefono', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('titulo');
-            $table->string('texto');
-            $table->integer('idUsuario')->unsigned();
-            $table->foreign('idUsuario') ->references('id')->on('users');
-            $table->string('fechaDeValides')->nullable();
+            $table->string('nombre');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -32,6 +28,6 @@ class Promociones extends Migration
      */
     public function down()
     {
-        Schema::drop('noticias');
+        Schema::dropIfExists('tipotelefono');
     }
 }
