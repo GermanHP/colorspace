12b8890d9c280ab95ee8e72c61fba6a1bc72119a
ajla95 <josephe95@hotmail.com>
2017-09-12 00:27:31 -0600

    Cambios hechos el portafolio version 1.1 agregadas las descripciones(con error) de las imagenes

--- resources/views/portfolios/verMas/arquiMas.blade.php
+++ resources/views/portfolios/verMas/arquiMas.blade.php
@@ -4,22 +4,37 @@
     <div class="row" id="">
         <br><br>
 
-        <div class="col-lg-4">
+        <div class="col-lg-4 col-sm-12">
             <div class="porta-img">
                 <img src="/img/porta/arqui/01adidasMetro.jpg">
+            </div>
+
+            <div class="descrip "> <!-- este sera el prueba de descripcion-->
+                <h2>Titulo</h2>
+                <h4>esta sera la descripcion</h4>
 
             </div>
         </div>
 
-        <div class="col-lg-4">
+        <div class="col-lg-4 col-sm-12">
             <div class="porta-img">
             <img src="/img/porta/arqui/02adidasMulti.jpg">
             </div>
+            <div class="descrip "> <!-- este sera el prueba de descripcion-->
+                <h2>Titulo</h2>
+                <h4>esta sera la descripcion 2</h4>
+
+            </div>
         </div>
 
-        <div class="col-lg-4">
+        <div class="col-lg-4 col-sm-12">
             <div class="porta-img">
                 <img src="/img/porta/arqui/03adidasPerMulti.jpg">
+            </div>
+            <div class="descrip "> <!-- este sera el prueba de descripcion-->
+                <h2>Titulo</h2>
+                <h4>esta sera la descripcion03</h4>
+
             </div>
 
         </div>
@@ -30,21 +45,36 @@
     <div class="row">
 
         
-        <div class="col-lg-4">
+        <div class="col-lg-4 col-sm-12">
             <div class="porta-img">
                 <img src="/img/porta/arqui/04flexiSanMi.jpg">
             </div>
-        </div>
+            <div class="descrip "> <!-- este sera el prueba de descripcion-->
+                <h2>Titulo</h2>
+                <h4>esta sera la descripcion 04</h4>
 
-        <div class="col-lg-4" >
-            <div class="porta-img">
-                <img src="/img/porta/arqui/05reboMulti.jpg">
             </div>
         </div>
 
-        <div class="col-lg-4">
+        <div class="col-lg-4 col-sm-12" >
+            <div class="porta-img">
+                <img src="/img/porta/arqui/05reboMulti.jpg">
+            </div>
+            <div class="descrip "> <!-- este sera el prueba de descripcion-->
+                <h2>Titulo</h2>
+                <h4>esta sera la descripcion 05</h4>
+
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-sm-12">
             <div class="porta-img">
                 <img src="/img/porta/arqui/06carcamo.jpg">
+
+            </div>
+            <div class="descrip "> <!-- este sera el prueba de descripcion-->
+                <h2>Titulo</h2>
+                <h4>esta sera la descripcion06</h4>
 
             </div>
 
@@ -54,22 +84,37 @@
 
     <div class="row">
 
-        <div class="col-lg-4">
+        <div class="col-lg-4 col-sm-12">
             <div class="porta-img">
                 <img src="/img/porta/arqui/07atacoResi.jpg">
             </div>
+            <div class="descrip "> <!-- este sera el prueba de descripcion-->
+                <h2>Titulo</h2>
+                <h4>esta sera la descripcion07</h4>
+
+            </div>
          </div>
 
-        <div class="col-lg-4">
+        <div class="col-lg-4 col-sm-12">
             <div class="porta-img">
                 <img src="/img/porta/arqui/07caraSuResi.jpg">
             </div>
+            <div class="descrip "> <!-- este sera el prueba de descripcion-->
+                <h2>Titulo</h2>
+                <h4>esta sera la descripcion 08</h4>
+
+            </div>
         </div>
 
-       <div class="col-lg-4">
+       <div class="col-lg-4 col-sm-12">
             <div class="porta-img">
                 <img src="/img/porta/arqui/08cenHallCali.jpg">
             </div>
+           <div class="descrip "> <!-- este sera el prueba de descripcion-->
+               <h2>Titulo</h2>
+               <h4>esta sera la descripcion 09</h4>
+
+           </div>
         </div>
     </div>
 
