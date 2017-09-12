4f84b2c5a966fcb73658cd10978d85ea520ed284
ajla95 <josephe95@hotmail.com>
2017-09-12 15:54:28 -0600

    Cambios hechos el portafolio version 1.2
     c mamo esta version XD aparecia barra de desplazamiento lateral extra XD

--- resources/views/portfolios/verMas/arquiMas.blade.php
+++ resources/views/portfolios/verMas/arquiMas.blade.php
@@ -1,37 +1,38 @@
 @extends('layouts.app')
 @section('content')
 
-    <div class="row" id="">
+
+    <!--<div class="row" >
         <br><br>
 
-        <div class="col-lg-4 col-sm-12">
+        <div class="col-lg-4 ">
             <div class="porta-img">
                 <img src="/img/porta/arqui/01adidasMetro.jpg">
             </div>
 
-            <div class="descrip "> <!-- este sera el prueba de descripcion-->
+            <div class="descrip "> <!-- este sera el prueba de descripcion
                 <h2>Titulo</h2>
                 <h4>esta sera la descripcion</h4>
 
             </div>
         </div>
 
-        <div class="col-lg-4 col-sm-12">
+        <div class="col-lg-4 ">
             <div class="porta-img">
             <img src="/img/porta/arqui/02adidasMulti.jpg">
             </div>
-            <div class="descrip "> <!-- este sera el prueba de descripcion-->
+            <div class="descrip "> <!-- este sera el prueba de descripcion-
                 <h2>Titulo</h2>
                 <h4>esta sera la descripcion 2</h4>
 
             </div>
         </div>
 
-        <div class="col-lg-4 col-sm-12">
+        <div class="col-lg-4 ">
             <div class="porta-img">
                 <img src="/img/porta/arqui/03adidasPerMulti.jpg">
             </div>
-            <div class="descrip "> <!-- este sera el prueba de descripcion-->
+            <div class="descrip "> <!-- este sera el prueba de descripcion--
                 <h2>Titulo</h2>
                 <h4>esta sera la descripcion03</h4>
 
@@ -41,38 +42,39 @@
 
 
     </div>
-    
+
+
     <div class="row">
 
         
-        <div class="col-lg-4 col-sm-12">
+        <div class="col-lg-4 ">
             <div class="porta-img">
                 <img src="/img/porta/arqui/04flexiSanMi.jpg">
             </div>
-            <div class="descrip "> <!-- este sera el prueba de descripcion-->
+            <div class="descrip "> <!-- este sera el prueba de descripcion--
                 <h2>Titulo</h2>
                 <h4>esta sera la descripcion 04</h4>
 
             </div>
         </div>
 
-        <div class="col-lg-4 col-sm-12" >
+        <div class="col-lg-4 " >
             <div class="porta-img">
                 <img src="/img/porta/arqui/05reboMulti.jpg">
             </div>
-            <div class="descrip "> <!-- este sera el prueba de descripcion-->
+            <div class="descrip "> <!-- este sera el prueba de descripcion--
                 <h2>Titulo</h2>
                 <h4>esta sera la descripcion 05</h4>
 
             </div>
         </div>
 
-        <div class="col-lg-4 col-sm-12">
+        <div class="col-lg-4 ">
             <div class="porta-img">
                 <img src="/img/porta/arqui/06carcamo.jpg">
 
             </div>
-            <div class="descrip "> <!-- este sera el prueba de descripcion-->
+            <div class="descrip "> <!-- este sera el prueba de descripcion--
                 <h2>Titulo</h2>
                 <h4>esta sera la descripcion06</h4>
 
@@ -82,41 +84,48 @@
         
     </div>
 
+
+
     <div class="row">
 
-        <div class="col-lg-4 col-sm-12">
+        <div class="col-lg-4 ">
             <div class="porta-img">
                 <img src="/img/porta/arqui/07atacoResi.jpg">
             </div>
-            <div class="descrip "> <!-- este sera el prueba de descripcion-->
+            <div class="descrip "> <!-- este sera el prueba de descripcion--
                 <h2>Titulo</h2>
                 <h4>esta sera la descripcion07</h4>
 
             </div>
          </div>
 
-        <div class="col-lg-4 col-sm-12">
+        <div class="col-lg-4 ">
             <div class="porta-img">
                 <img src="/img/porta/arqui/07caraSuResi.jpg">
             </div>
-            <div class="descrip "> <!-- este sera el prueba de descripcion-->
+            <div class="descrip "> <!-- este sera el prueba de descripcion--
                 <h2>Titulo</h2>
                 <h4>esta sera la descripcion 08</h4>
 
             </div>
         </div>
 
-       <div class="col-lg-4 col-sm-12">
+       <div class="col-lg-4 ">
             <div class="porta-img">
                 <img src="/img/porta/arqui/08cenHallCali.jpg">
             </div>
-           <div class="descrip "> <!-- este sera el prueba de descripcion-->
+           <div class="descrip "> <!-- este sera el prueba de descripcion--
                <h2>Titulo</h2>
                <h4>esta sera la descripcion 09</h4>
 
            </div>
         </div>
     </div>
+-->
+
+    <!-- esta es una segunda prueba a ver que mamada movi XD-->
+
+    <>
 
 
 
