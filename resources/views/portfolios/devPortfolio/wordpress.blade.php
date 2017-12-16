e820d373dc433aa74f6b516cd7648386b8d76642
Gabriel Abrego <gabrielblanco.gabo@gmial.com>
2017-12-15 21:11:30 -0600

    portfolios 50%

--- resources/views/portfolios/devPortfolio/wordpress.blade.php
+++ resources/views/portfolios/devPortfolio/wordpress.blade.php
@@ -4,38 +4,38 @@
     <div class="row">
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4" >
 
-            <img class="imagen-porta-arqui" src="img/porta/arqui/01adidasMetro.jpg" alt="">
+            <img class="imagen-porta-arqui" src="img/porta/dev/1.jpg" alt="">
         </div>
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
             <a href="#" data-toggle="modal" data-target="#modalColegio">
-                <img class="imagen-porta-arqui" src="img/porta/arqui/02adidasMulti.jpg" alt="">
+                <img class="imagen-porta-arqui" src="img/porta/dev/1.jpg" alt="">
             </a>
         </div>
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
             <a href="img/porta/arqui/03adidasPerMulti.jpg " data-toggle="lightbox">
-                <img class="imagen-porta-arqui" src="img/porta/arqui/03adidasPerMulti.jpg" alt="">
+                <img class="imagen-porta-arqui" src="img/porta/dev/1.jpg" alt="">
             </a>
         </div>
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-            <img class="imagen-porta-arqui" src="img/porta/arqui/04flexiSanMi.jpg" alt="">
+            <img class="imagen-porta-arqui" src="img/porta/dev/1.jpg" alt="">
         </div>
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-            <img class="imagen-porta-arqui" src="img/porta/arqui/05reboMulti.jpg" alt="">
+            <img class="imagen-porta-arqui" src="img/porta/dev/1.jpg" alt="">
         </div>
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-            <img class="imagen-porta-arqui" src="img/porta/arqui/06carcamo.jpg" alt="">
+            <img class="imagen-porta-arqui" src="img/porta/dev/1.jpg" alt="">
 
         </div>
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-            <img class="imagen-porta-arqui" src="img/porta/arqui/07atacoResi.jpg" alt="">
+            <img class="imagen-porta-arqui" src="img/porta/dev/1.jpg" alt="">
 
         </div>
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-            <img class="imagen-porta-arqui" src="img/porta/arqui/08cenHallCali.jpg" alt="">
+            <img class="imagen-porta-arqui" src="img/porta/dev/1.jpg" alt="">
 
         </div>
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-            <img class="imagen-porta-arqui" src="img/porta/arqui/09lucesResi.jpg" alt="">
+            <img class="imagen-porta-arqui" src="img/porta/dev/1.jpg" alt="">
         </div>
     </div>
 
