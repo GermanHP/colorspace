c993c80d82ec6dc2e50ccfa372559e4dc20ac46d
Gabriel Abrego <gabrielblanco.gabo@gmial.com>
2018-01-16 16:49:32 -0600

    portfolios arquitectura 100%

--- resources/views/portfolios/arquiPortfolio/arquitectonico.blade.php
+++ resources/views/portfolios/arquiPortfolio/arquitectonico.blade.php
@@ -4,46 +4,47 @@
     <div class="row">
         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
             <a href="{{url('/adidas-metrocentro')}}">
-                <img class="imagen-porta-arqui" src="img/porta/arqui/01adidasMetro.jpg" alt="">
+                <img class="imagen-porta-arqui" src="img/porta/arqui/1.jpg" alt="">
                 <h3 id="arquitectura" class="text-center white-text"><span>Adidas Core Metrocentro</span></h3><br>
                 <h5 id="arquitectura" class="text-center up-inter white-text"><span>{{trans('arqui.supConst')}}</span></h5><br>
             </a>
         </div>
         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
             <a href="{{url('/adidas-multiplaza')}}">
-                <img class="imagen-porta-arqui" src="img/porta/arqui/02adidasMulti.jpg" alt="">
+                <img class="imagen-porta-arqui" src="img/porta/arqui/2.jpg" alt="">
                 <h3 id="arquitectura" class="text-center white-text"><span>Adidas Originals Multiplaza</span></h3><br>
                 <h5 id="arquitectura" class="text-center up-inter white-text"><span>{{trans('arqui.supConst')}}</span></h5><br>
             </a>
         </div>
         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
             <a href="{{url('/adidas-performance-multiplaza')}}">
-                <img class="imagen-porta-arqui" src="img/porta/arqui/03adidasPerMulti.jpg" alt="">
+                <img class="imagen-porta-arqui" src="img/porta/arqui/3.jpg" alt="">
                 <h3 id="arquitectura" class="text-center white-text"><span>Adidas Performance Multiplaza</span></h3><br>
                 <h5 id="arquitectura" class="text-center up-inter white-text"><span>{{trans('arqui.supConst')}}</span></h5><br>
             </a>
         </div>
         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
             <a href="{{url('/flexi-sanmiguel')}}">
-                <img class="imagen-porta-arqui" src="img/porta/arqui/04flexiSanMi.jpg" alt="">
+                <img class="imagen-porta-arqui" src="img/porta/arqui/4.jpg" alt="">
                 <h3 id="arquitectura" class="text-center white-text"><span>Flexi San Miguel</span></h3><br>
                 <h5 id="arquitectura" class="text-center up-inter white-text"><span>{{trans('arqui.supConst')}}</span></h5><br>
             </a>
         </div>
         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
             <a href="{{url('/reebok-multiplaza')}}">
-                <img class="imagen-porta-arqui" src="img/porta/arqui/05reboMulti.jpg" alt="">
+                <img class="imagen-porta-arqui" src="img/porta/arqui/5.jpg" alt="">
                 <h3 id="arquitectura" class="text-center white-text"><span>Rebook Multiplaza</span></h3><br>
                 <h5 id="arquitectura" class="text-center up-inter white-text"><span>{{trans('arqui.supConst')}}</span></h5><br>
             </a>
         </div>
         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
             <a href="{{url('/carcamo-naranjos')}}">
-                <img class="imagen-porta-arqui" src="img/porta/arqui/06carcamo.jpg" alt="">
+                <img class="imagen-porta-arqui" src="img/porta/arqui/6.jpg" alt="">
                 <h3 id="arquitectura" class="text-center white-text"><span>Carcamo los naranjos</span></h3><br>
                 <h5 id="arquitectura" class="text-center up-inter white-text"><span>{{trans('arqui.desSupCons')}}</span></h5><br>
             </a>
         </div>
+        <!--
         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
             <a href="{{url('/ataco-residencia')}}">
                 <img class="imagen-porta-arqui" src="img/porta/arqui/07atacoResi.jpg" alt="">
@@ -65,6 +66,7 @@
                 <h5 id="arquitectura" class="text-center up-inter white-text"><span>{{trans('arqui.desArqui')}}</span></h5><br>
             </a>
         </div>
+        -->
     </div>
 
 @stop
