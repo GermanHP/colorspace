dc38709a45f97499e3472cdfef0d742b0e719f4e
Gabriel Abrego <gabrielblanco.gabo@gmial.com>
2018-01-31 19:52:55 -0600

    portfolios arquitectura, Diseño y Desarrollo 100%

---
+++ resources/views/portfolios/devPortfolio/slidersDev/customweb.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.sliders')
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="carousel2"
+                 data-flickity='{ "freeScroll": true, "contain": true, "prevNextButtons": true, "pageDots": false, "imagesLoaded": true }'>
+                <div class="carousel-cell" style="background: white;">
+                    <div class="texto-slider centrado-slider">
+                        <h2 class="text-center text-uppercase">Porfolio Web/Android Development</h2>
+                        <h4 class="text-center show-on-small">Desliza para más información</h4>
+                    </div>
+                </div>
+                <img src="img/porta/dev/customwebs/1.jpg" alt="web" />
+                <img src="img/porta/dev/customwebs/2.jpg" alt="web" />
+                <img src="img/porta/dev/customwebs/3.jpg" alt="web" />
+                <img src="img/porta/dev/customwebs/4.jpg" alt="web" />
+                <img src="img/porta/dev/customwebs/5.jpg" alt="web" />
+                <img src="img/porta/dev/customwebs/6.jpg" alt="web" />
+                <img src="img/porta/dev/customwebs/7.jpg" alt="web" />
+                <img src="img/porta/dev/customwebs/8.jpg" alt="web" />
+                <img src="img/porta/dev/customwebs/9.jpg" alt="web" />
+                <img src="img/porta/dev/customwebs/10.jpg" alt="web" />
+                <img src="img/porta/dev/customwebs/11.jpg" alt="web" />
+                <img src="img/porta/dev/customwebs/12.jpg" alt="web" />
+                <img src="img/porta/dev/customwebs/13.jpg" alt="web" />
+            </div>
+        </div>
+
+    </div>
+@stop
