bad2c678a731520c724e63847b645d41817db102
Gabriel Abrego <gabrielblanco.gabo@gmial.com>
2018-02-03 15:53:02 -0600

    arreglo en sliders arquitectura

--- resources/views/portfolios/arquiPortfolio/slidersArqui/carcamo.blade.php
+++ resources/views/portfolios/arquiPortfolio/slidersArqui/carcamo.blade.php
@@ -3,10 +3,10 @@
     <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="carousel2"
-                 data-flickity='{ "freeScroll": true, "contain": true, "prevNextButtons": true, "pageDots": false, "imagesLoaded": true }'>
+                 data-flickity='{ "freeScroll": true, "contain": true, "prevNextButtons": true, "pageDots": false, "imagesLoaded": true, "initialIndex": 0 }'>
                 <div class="carousel-cell" style="background: white;">
                     <div class="texto-slider centrado-slider">
-                        <h2 class="text-center text-uppercase">Carcamo los naranjo</h2>
+                        <h2 class="text-center text-uppercase">Carcamo los Naranjo</h2>
                         <h3 class="text-center"><span>{{trans('arqui.desSupCons')}}</span></h3>
                         <h4 class="text-center mobil-only">Desliza para más información</h4>
                     </div>
