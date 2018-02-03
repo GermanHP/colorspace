4e62ef74e7dbc1f59bf0bec81bd2915f8fea9b0b
Gabriel Abrego <gabrielblanco.gabo@gmial.com>
2018-02-03 15:55:04 -0600

    arreglo en sliders desing

--- resources/views/portfolios/desingPortfolio/slidersDesing/papeleria.blade.php
+++ resources/views/portfolios/desingPortfolio/slidersDesing/papeleria.blade.php
@@ -3,7 +3,7 @@
     <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="carousel2"
-                 data-flickity='{ "freeScroll": true, "contain": true, "prevNextButtons": true, "pageDots": false, "imagesLoaded": true }'>
+                 data-flickity='{ "freeScroll": true, "contain": true, "prevNextButtons": true, "pageDots": false, "imagesLoaded": true, "initialIndex": 0 }'>
                 <div class="carousel-cell" style="background: white;">
                     <div class="texto-slider centrado-slider">
                         <h2 class="text-center text-uppercase">Papeleria</h2>
