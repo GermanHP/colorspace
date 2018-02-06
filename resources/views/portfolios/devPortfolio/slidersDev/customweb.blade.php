01d79eee400c85479759a8f9f999e727440610a4
Gabriel Abrego <gabrielblanco.gabo@gmial.com>
2018-02-05 19:32:44 -0600

    arreglo en sliders diseño

--- resources/views/portfolios/devPortfolio/slidersDev/customweb.blade.php
+++ resources/views/portfolios/devPortfolio/slidersDev/customweb.blade.php
@@ -3,28 +3,40 @@
     <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="carousel2"
-                 data-flickity='{ "freeScroll": true, "contain": true, "prevNextButtons": true, "pageDots": false, "imagesLoaded": true }'>
-                <div class="carousel-cell" style="background: white;">
-                    <div class="texto-slider centrado-slider">
-                        <h2 class="text-center text-uppercase">Porfolio Web/Android Development</h2>
-                        <h4 class="text-center visible-xs-block hidden-xs">Desliza para más información</h4>
+                 data-flickity='{"contain": true, "prevNextButtons": true, "pageDots": false, "imagesLoaded": true }'>
+                <div class="carousel-cell" style="background: #1A3C58;">
+                    <div class="texto-slider centrado-slider white-text">
+                        <h2 class="text-center text-uppercase">Web Development</h2>
+                        <h4 class="text-center visible-xs ext-uppercase"><span>{{trans('arqui.desIz')}}</span></h4>
                     </div>
                 </div>
-                <img src="img/porta/dev/customwebs/1.jpg" alt="web" />
-                <img src="img/porta/dev/customwebs/2.jpg" alt="web" />
                 <img src="img/porta/dev/customwebs/3.jpg" alt="web" />
                 <img src="img/porta/dev/customwebs/4.jpg" alt="web" />
                 <img src="img/porta/dev/customwebs/5.jpg" alt="web" />
                 <img src="img/porta/dev/customwebs/6.jpg" alt="web" />
                 <img src="img/porta/dev/customwebs/7.jpg" alt="web" />
                 <img src="img/porta/dev/customwebs/8.jpg" alt="web" />
-                <img src="img/porta/dev/customwebs/9.jpg" alt="web" />
-                <img src="img/porta/dev/customwebs/10.jpg" alt="web" />
-                <img src="img/porta/dev/customwebs/11.jpg" alt="web" />
-                <img src="img/porta/dev/customwebs/12.jpg" alt="web" />
-                <img src="img/porta/dev/customwebs/13.jpg" alt="web" />
             </div>
         </div>
-
+    </div>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="carousel2"
+                 data-flickity='{"contain": true, "prevNextButtons": true, "pageDots": false, "imagesLoaded": true }'>
+                <div class="carousel-cell" style="background: white;">
+                    <div class="texto-slider centrado-slider cs-text">
+                        <h2 class="text-center text-uppercase">Android Development</h2>
+                        <h4 class="text-center visible-xs ext-uppercase"><span>{{trans('arqui.desIz')}}</span></h4>
+                    </div>
+                </div>
+                <img src="img/porta/dev/customwebs/1.jpg" alt="android" />
+                <img src="img/porta/dev/customwebs/2.jpg" alt="android" />
+                <img src="img/porta/dev/customwebs/9.jpg" alt="android" />
+                <img src="img/porta/dev/customwebs/10.jpg" alt="android" />
+                <img src="img/porta/dev/customwebs/11.jpg" alt="android" />
+                <img src="img/porta/dev/customwebs/12.jpg" alt="android" />
+                <img src="img/porta/dev/customwebs/13.jpg" alt="android" />
+            </div>
+        </div>
     </div>
 @stop
